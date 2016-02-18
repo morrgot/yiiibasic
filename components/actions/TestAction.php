6cc22f2d08ae73f0f5662ba1471ee4e7d1e4d67f
Alex Ponirovskiy <ponirovskalex@gmail.com>
2016-02-18 17:34:33 +0200

    test action

---
+++ components/actions/TestAction.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aponirovskiy
+ * Date: 18.02.2016
+ * Time: 17:26
+ */
+
+namespace app\components\actions;
+
+
+use yii\base\Action;
+
+class TestAction extends Action{
+
+    protected $start_time;
+
+    protected $end_time;
+
+    public function run()
+    {
+        echo 'adasdasdasd';
+    }
+
+    protected function beforeRun()
+    {
+        $this->start_time = microtime(true);
+        return true;
+    }
+
+    protected function afterRun()
+    {
+        $this->end_time = microtime(true);
+
+        echo "\n\n\n".round(abs($this->end_time - $this->start_time), 8);
+    }
+
+}
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -44,6 +44,9 @@ class SiteController extends Controller
                 'class' => 'yii\captcha\CaptchaAction',
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
+            'test' => [
+                'class' => 'app\components\actions\TestAction'
+            ]
         ];
     }
 
