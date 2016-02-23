1a3bbbc371468c9589ca7232839f5e5b20867547
Alex Ponirovskiy <ponirovskalex@gmail.com>
2016-02-23 18:57:08 +0200

    master commit

--- config/web.php
+++ config/web.php
@@ -34,7 +34,7 @@ $config = [
             'class' => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
+            // for the mailer to send real email s.
             'useFileTransport' => true,
         ],
         'log' => [
