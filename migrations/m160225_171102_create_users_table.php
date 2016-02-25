82c028426e01a3fcde674892ed26b2c0ff945619
Alex Ponirovskiy <ponirovskalex@gmail.com>
2016-02-25 19:53:43 +0200

    db migrations: saving migration to git

--- migrations/m160225_171102_create_users_table.php
+++ migrations/m160225_171102_create_users_table.php
@@ -1,18 +1,35 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 class m160225_171102_create_users_table extends Migration
 {
+    const USERS_TABLE_NAME = 'users';
+
     public function up()
     {
-        $this->createTable('users_table', [
-            'id' => $this->primaryKey()
-        ]);
+
     }
 
     public function down()
     {
-        $this->dropTable('users_table');
+        $this->dropTable(self::USERS_TABLE_NAME);
     }
+
+    public function safeUp()
+    {
+        $this->createTable(self::USERS_TABLE_NAME, [
+            'id' => $this->primaryKey(),
+            'email' => $this->string(40)->notNull()->unique(),
+            'password' => Schema::TYPE_STRING
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable(self::USERS_TABLE_NAME);
+    }
+
+
 }
